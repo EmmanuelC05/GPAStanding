9b12f1d625bd2126901dfde82db68e47944c3ac0
ARey-H39 <127042817+ARey-H39@users.noreply.github.com>
2023-12-06 16:40:12 -0600

    Add tests

--- tests/GPATest.java
+++ tests/GPATest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,11 +6,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class GPATest {
 
     @Test
-    void checkNeedsHelp() {
-        assertAll(() -> {
-            assertEquals("Needs Help", GPA.checkStanding(1.7)),
-                    assertEquals("Needs Help", GPA.checkStanding(1.5)),
-                    assertEquals("Needs Help", GPA.checkStanding(1.2))
-        });
+    @DisplayName("Convert grade D to 1.0")
+    void testGradeConversion2() {
+        GPA calc = new GPA();
+        assertEquals(1.0, calc.convertGrade("D"));
+    }
+
+    @Test
+    @DisplayName("Convert grade D+ to 1.33")
+    void testGradeConversion3() {
+        GPA calc = new GPA();
+        assertEquals(1.33, calc.convertGrade("D+"));
+    }
+
+    @Test
+    @DisplayName("Convert grade D- to 0.67")
+    void testGradeConversion4() {
+        GPA calc = new GPA();
+        assertEquals(0.67, calc.convertGrade("D-"));
+    }
+
+    @Test
+    @DisplayName("Convert grade B- to 1.0")
+    void testGradeConversion5() {
+        GPA calc = new GPA();
+        assertEquals(2.67, calc.convertGrade("B-"));
+    }
+
+    @Test
+    @DisplayName("Convert grade F to 0.0")
+    void testGradeConversion6() {
+        GPA calc = new GPA();
+        assertEquals(0.0, calc.convertGrade("F"));
     }
 }
