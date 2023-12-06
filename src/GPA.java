53317485e0e9de3d94f8f8ce1d1021459793a6ae
ARey-H39 <127042817+ARey-H39@users.noreply.github.com>
2023-12-06 11:26:47 -0600

    Reformat code in GPA.java

--- src/GPA.java
+++ src/GPA.java
@@ -1,7 +1,13 @@
 public class GPA {
     public static String checkStanding(double gpa) {
-        if (gpa < 1.7) return "Needs Help";
-        else if (gpa > 1.7 || gpa < 3.0) return "Good Standing";
-        else return "Very Good Standing";
+        if (gpa < 1.7) {
+            return "Needs Help";
+        }
+        else if (gpa > 1.7 || gpa < 3.0) {
+            return "Good Standing";
+        }
+        else {
+            return "Very Good Standing";
+        }
     }
 }
