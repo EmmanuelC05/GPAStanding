09a9c32b08a5f209ec01d3d5f8a0c78ed3f1795f
ARey-H39 <127042817+ARey-H39@users.noreply.github.com>
2023-12-06 13:01:31 -0600

    Update program files

--- src/GPA.java
+++ src/GPA.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class GPA {
+    ArrayList<Double> gradesList = new ArrayList<>();
+
     public static String checkStanding(double gpa) {
         if (gpa < 1.7) {
             return "Needs Help";
@@ -9,5 +14,56 @@ public class GPA {
         else {
             return "Very Good Standing";
         }
+    }
+
+    public static boolean verifyGPA(double gpa) {
+        return !((gpa < 0) || (gpa > 4));
+    }
+
+    public double calculateGPA() {
+        double totalSum = 0.0;
+        for (double grade : gradesList) totalSum += grade;
+        return totalSum / gradesList.size();
+    }
+
+    public double convertGrade(String grade) {
+        HashMap <String, Double> gradeValue = new HashMap<>();
+
+        gradeValue.put("f", 4.0);
+        gradeValue.put("d-", 6.7);
+        gradeValue.put("d", 1.0);
+        gradeValue.put("d+", 1.33);
+        gradeValue.put("c-", 1.67);
+        gradeValue.put("c", 2.0);
+        gradeValue.put("c+", 2.33);
+        gradeValue.put("b-", 2.67);
+        gradeValue.put("b", 3.0);
+        gradeValue.put("b+", 3.33);
+        gradeValue.put("a-", 3.67);
+        gradeValue.put("a", 4.0);
+
+        grade = grade.toLowerCase();
+
+        if (gradeValue.containsKey(grade)) return gradeValue.get(grade);
+
+        throw new IllegalArgumentException("Invalid grade: " +
+                "possible choices are A, A-, B+, B, B-, C+, C, C-, D+, D, D-, F");
+    }
+
+    public void addGrade(String grade) {
+        gradesList.add(convertGrade(grade));
+    }
+
+    public String printGradesList() {
+        String output = "";
+
+        for (double grade : gradesList) {
+            output += grade + " ";
+        }
+
+        output = output.trim();
+        output = output.replace(" ", ", ");
+
+        return output;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,79 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        boolean runProgram = true;
+        GPA gpaCalculator = new GPA();
+
+        while (runProgram) {
+            Scanner keyboard = new Scanner(System.in);
+            int input;
+
+            System.out.println("GPA Calculator");
+            System.out.println("--------------");
+            System.out.println("1) Add grade");
+            System.out.println("2) Calculate GPA");
+            System.out.println("3) Check GPA Standing");
+            System.out.println("4) Exit\n");
+
+            System.out.print("Enter an option (1-4): ");
+            input = keyboard.nextInt();
+
+            if (input == 1)
+                addGrade(gpaCalculator);
+            else if (input == 2)
+                calculateGPA(gpaCalculator);
+            else if (input == 3)
+                checkStanding(gpaCalculator);
+            else if (input == 4)
+                runProgram = false;
+            else
+                System.out.println("* Invalid input\n");
+        }
+
+
+    }
+
+    public static void addGrade(GPA calc) {
+        Scanner keyboard = new Scanner(System.in);
+        String input;
+
+        System.out.println("GPA Calculator");
+        System.out.println("--------------");
+        System.out.println("Current grades: " + calc.printGradesList());
+        System.out.print("Enter a grade: ");
+        input = keyboard.nextLine();
+
+        try {
+            calc.addGrade(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println();
+    }
+
+    public static void calculateGPA(GPA calc) {
+        System.out.println("GPA Calculator");
+        System.out.println("--------------");
+        System.out.println("Current grades: " + calc.printGradesList());
+        System.out.println("GPA: " + calc.calculateGPA());
+        System.out.println();
+    }
+
+    public static void checkStanding(GPA calc) {
         Scanner keyboard = new Scanner(System.in);
         double input;
 
-        System.out.printf("Enter your GPA: ");
+        System.out.println("GPA Calculator");
+        System.out.println("--------------");
+        System.out.print("Enter your GPA: ");
         input = keyboard.nextDouble();
 
-        System.out.println(GPA.checkStanding(input));
+        if (GPA.verifyGPA(input))
+            System.out.println(GPA.checkStanding(input));
+        else
+            System.out.println("Invalid input");
+
+        System.out.println();
     }
-
-
 }
